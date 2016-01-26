5bb6dd0ed29b6d2a26a5463c0a90e241db3c761a
patriciocordeiro <paponcio6@gmail.com>
2016-01-26 19:19:33 -0300

    Acrescentado lodash, categoria e subcategoria

--- public/components/products/products.ctrl.js
+++ public/components/products/products.ctrl.js
@@ -9,16 +9,34 @@
         console.log(ProductSrvc);
         console.log('Product controller running');
         vm.newProduct = '';
-        vm.fabricante = ['Philips', 'Samsung', 'Sony', 'Bourns'];
+        vm.fabricante = ['Philips', 'Samsung', 'Sony', 'Bourns']
+        vm.categoria = ['componentes passivos', 'Semicondutores', 'Optoeletronicos', 'conversores', 'placas de desenvolvimento', 'kits ditáticos', 'microprocessadores', 'microcontroladores']
 
 
+        vm.subcategorias = 
+            [{
+                categoria: 'componentes passivos',
+                items: ['resistor', 'capacitor']
 
+            },{
+                categoria: 'conversores',
+                items: ['DAC', 'ADC']
+
+            }]
+        
         vm.getProductReg = function(newProduct) {
             console.log(newProduct);
             ProductSrvc.prd.register(newProduct, function(data) {
                 console.log(data);
             })
 
+        }
+
+        vm.getCategory = function(category) {
+           var idx = _.findIndex(vm.subcategorias, {categoria:category})
+            vm.seleSubcat =vm.subcategorias[idx]
+            
+            console.log(vm.seleSubcat);
         }
     }
 
